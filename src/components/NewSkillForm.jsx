98d22f8b97094fd0db10ea5342069ba7ca4192b5
Wan Aqim <wanaqim@gmail.com>
2023-11-15 17:16:15 +0800

    add skill function

--- src/components/NewSkillForm.jsx
+++ src/components/NewSkillForm.jsx
@@ -1,19 +1,43 @@
-function NewSkillForm() {
-    return(
+import {useState} from "react";
 
-        <form className="NewSkillForm">
+function NewSkillForm( {addSkill} ) {
+    const [formData, setFormData] = useState({
+        skill: " ",
+        level: " "
+    });
+    function handleChange(event) {
+        const newFormData = {[event.target.name]: event.target.value };
+        setFormData(newFormData);
+    }
+    function addNewSkill(event) {
+        event.preventDefault();
+        addSkill(formData);
+    }
+    return(
+        <div>
+        <form className="NewSkillForm" onSubmit={addNewSkill}>
             <label>Skill</label>
-            <input type="text" name="" id="" />
+            <input 
+            type="text" 
+            name="skill"  
+            onChange={handleChange}
+            /> 
             <label>Level</label>
-            <select name="level">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
+            <select 
+            name="level"
+            onChange={handleChange}
+            >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
             </select>
             <button type="submit">ADD SKILL</button>
         </form>
+        <h1>{formData.skill} {formData.level}</h1>
+        </div>
+        
     );
 };
 
